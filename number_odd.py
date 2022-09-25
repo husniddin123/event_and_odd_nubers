f2ca2439866a24b836b7106ef6b5cc3cec347e70
husniddin <baxodirovhusniddin66@gemal.com>
2022-09-25 13:11:47 +0500

    bulafugyugy

--- number_odd.py
+++ number_odd.py
@@ -3,22 +3,18 @@ number = 5438
 counter = 0 
 
 x1 = number % 10 
-x1 += 1
 counter += x1 % 2
 number //= 10
 
 x2 = number % 10 
-x2 += 1
 counter += x2 % 2
 number //= 10
 
 x3 = number % 10 
-x3 += 1
 counter += x3 % 2
 number //= 10
 
 x4 = number % 10 
-x4 += 1
 counter += x4 % 2
 number //= 10
 #Create a variable "var_int" and assign it a four-digit integer value.
